7a9c3c59c0c4ff834eea0663e042ff7ef8dccd79
Fadekemi Zainab Haruna <fadekemizharuna@Fadekemis-MacBook-Pro.local>
2025-08-21 18:53:06 +0100

    2nd commit

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 
 const char* ssid = "Deke";
 const char* password = "tgyo3978";
-const char* FIRMWARE_VERSION = "1.0.1"; 
+const char* FIRMWARE_VERSION = "1.0.2"; 
 const char* versionFileUrl   = "https://raw.githubusercontent.com/Deke1604/Raw2/refs/heads/main/version.txt";
 const char* firmwareURL      = "https://github.com/Deke1604/Raw2/raw/main/firmware.bin";
 
@@ -15,7 +15,7 @@ WebServer server(80);
 const char* host = "esp32";
 
 unsigned long previousMillis = 0;
-const long interval = 500; // blink interval (1 sec)
+const long interval = 100; // blink interval (1 sec)
 bool ledState = LOW;
 const int led = 2; 
 
